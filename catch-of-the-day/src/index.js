e042bb8458dd60e96ee8284e05ffebbe65eb27ea
Alex Lementuev <a.lementuev@gmail.com>
2017-04-12 22:58:17 -0700

    Lessons 1-5

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
 // let's go!
+import React from 'react';
+import StorePicker from './components/StorePicker'
+import { render } from 'react-dom';
+import './css/style.css'
+
+render(<StorePicker/>, document.querySelector('#main'));
