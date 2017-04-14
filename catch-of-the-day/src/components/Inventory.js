d9a6e032eaf7284085931b82f879dd99198f2bbe
Alex Lementuev <a.lementuev@gmail.com>
2017-04-14 15:43:33 -0700

    Lessons 11-13

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component {
   render() {
     return (
-      <p>Inventory</p>
+      <div>
+        <h1>Inventory</h1>
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
     )
   }
 }
